3c080ca56d504f16a22acdcbe48532797a34868e
Chad <ohunome@sbcglobal.net>
2015-10-31 13:02:15 +0000

    latest

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -2,6 +2,10 @@
 
 (function () {
 
+
+   var addCat = document.querySelector('#btn-addtext-cats');
+   var delCat = document.querySelector('#btn-deltext-cats');
+   var genPoll = document.querySelector('#btn-genpoll-cats');
    var addButton = document.querySelector('.btn-add');
    var deleteButton = document.querySelector('.btn-delete');
    var clickNbr = document.querySelector('#click-nbr');
@@ -13,9 +17,30 @@
    }
 
    ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount));
+   
+   addCat.addEventListener('click', function () {
+      var inputtxt = document.createElement("input");
+      inputtxt.type = "text";
+      inputtxt.placeholder = "Option ?";
+
+      var textcats = document.querySelector('#text-cats p');
+      textcats.appendChild(inputtxt);
+      textcats.appendChild(document.createElement("br"));
+      
+   }, false);
+   
+   delCat.addEventListener('click', function () {
+      var textcats = document.querySelector('#text-cats p');
+      textcats.removeChild(textcats.lastChild);
+      textcats.removeChild(textcats.lastChild);
+   }, false);
+   
+   genPoll.addEventListener('click', function () {
+      //console.log("test");
+   }, false);
 
    addButton.addEventListener('click', function () {
-
+      //console.log('test');
       ajaxFunctions.ajaxRequest('POST', apiUrl, function () {
          ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount);
       });
